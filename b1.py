4dddd4a2b0f1e9d92f33db762c4e7e3dd52e5fc3
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-16 17:22:46 +0530

    Create b1.py

---
+++ b1.py
@@ -0,0 +1,12 @@
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+a.sort()
+l=[]
+for i in a:
+    l.append(k-i)
+l.sort()
+print(l)
+l.remove(0)
+for i in range(3):
+    print(k-l[i],end=" ")
+             
