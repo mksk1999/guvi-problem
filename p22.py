1418b19f7e979a77fce81103293b07dd7ca8bada
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-08-10 17:33:42 +0530

    Create p22.py

---
+++ p22.py
@@ -0,0 +1,13 @@
+n=int(input())
+if(n==2 or n==1):
+    print("0")
+for i in range(2,n):
+    c=1
+    for j in range(2,n):
+        if(i%j==0):
+            c+=1
+    if(c==2):
+        print(i,end=" ")
+print(" ")
+
+    
